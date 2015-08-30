00fc0275bf9b1cd9059735df656fb54df39cba97
chandu kavar <cckavar@ymail.com>
2015-08-30 17:03:58 +0530

    private method getVolumeInBaseUnit extracted

--- src/main/quantity/Length.java
+++ src/main/quantity/Length.java
@@ -12,12 +12,12 @@ public class Length extends Quantity{
     }
     public static Length total(Length length1, Length length2, UnitOfLength expectedUnit) {
 
-        double length1InBaseUnit = length1.amount * length1.unit.getConversionValue();
-        double length2InBaseUnit = length2.amount * length2.unit.getConversionValue();
-        double totalLengthInBaseUnit = length1InBaseUnit + length2InBaseUnit;
-
+        double totalLengthInBaseUnit = getLengthInBaseUnit(length1) + getLengthInBaseUnit(length2);
         double totalLengthInExpectedUnit = totalLengthInBaseUnit/(expectedUnit.getConversionValue());
 
         return new Length(Util.roundOf(totalLengthInExpectedUnit),expectedUnit);
+    }
+    private static double getLengthInBaseUnit(Length length){
+        return length.amount * length.unit.getConversionValue();
     }
 }
--- src/main/quantity/Volume.java
+++ src/main/quantity/Volume.java
@@ -12,13 +12,12 @@ public class Volume extends Quantity{
 
     public static Volume total(Volume volume1, Volume volume2, UnitOfVolume expectedUnit) {
 
-
-        double volume1InBaseUnit = volume1.amount * volume1.unit.getConversionValue();
-        double volume2InBaseUnit = volume2.amount * volume2.unit.getConversionValue();
-        double totalVolumeInBaseUnit = volume1InBaseUnit + volume2InBaseUnit;
-
+        double totalVolumeInBaseUnit = getVolumeInBaseUnit(volume1) + getVolumeInBaseUnit(volume2);
         double totalValueInExpectedUnit = totalVolumeInBaseUnit/(expectedUnit.getConversionValue());
 
         return new Volume(Util.roundOf(totalValueInExpectedUnit),expectedUnit);
+    }
+    private static double getVolumeInBaseUnit(Volume volume){
+        return volume.amount * volume.unit.getConversionValue();
     }
 }
